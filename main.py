b27671a97285ff449eb2c7637e7c5c7cd8095ba1
polit-d <id.polit@gmail.com>
2026-08-21 02:51:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1322,8 +1322,7 @@ async def cmd_setanket(message: Message):
         message,
         event_type="Новая анкета",
         text=(
-            f"Игрок @{clean_username(message.from_user.username)}"
-            f"or 'без username'} добавил анкету."
+           f"Игрок @{clean_username(message.from_user.username) or 'без username'} добавил анкету."
         ),
         username=message.from_user.username,
         anketa_url=url
